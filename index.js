8135fad85b753fde1c2ee351850ced53cc4aaebe
mcgidoug <mcgidoug@gmail.com>
2022-05-20 14:58:20 -0500

    updated dotenv files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
 
 const cwd = process.cwd();
 
@@ -8,15 +8,15 @@ const PORT = process.env.port || 3001;
 const app = express();
 
 // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('01-Activities')
-  ? cwd.split('/01-Activities/')[1]
+const activity = cwd.includes("01-Activities")
+  ? cwd.split("/01-Activities/")[1]
   : cwd;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server for ${activity} running on port ${PORT}!`);
   });
